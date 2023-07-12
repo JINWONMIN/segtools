0c332b7632654d9d93f685cc203c4a0374777992
민진원 <94345086+JINWONMIN@users.noreply.github.com>
2023-07-12 09:13:21 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,334 +1,57 @@
-from fastapi import FastAPI, File, Form, UploadFile
-from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamPredictor
-import zipfile
-import numpy as np
-from io import BytesIO
-from PIL import Image
-from base64 import b64encode
-from pydantic import BaseModel
+from segment_anything import sam_model_registry, SamPredictor
+from domain.utils.utils import read_content
+from domain.sam import sam_router
 
-def pil_image_to_base64(image):
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = b64encode(buffered.getvalue()).decode("utf-8")
-    return img_str
-def read_content(file_path: str) -> str:
-    """read the content of target file
-    """
-    with open(file_path, 'r', encoding='utf-8') as f:
-        content = f.read()
-    return content
-
-sam_checkpoint = "models/sam_vit_h_4b8939.pth" # "sam_vit_h_4b8939.pth" or "sam_vit_l_0b3195.pth" or "sam_vit_b_01ec64.pth
-model_type = "vit_h" # "vit_l" or "vit_h"
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cuda:3"
-
-print("Loading model")
-SAM = sam_model_registry[model_type](checkpoint=sam_checkpoint).to(device)
-print("Finishing loading")
-
-predictor = SamPredictor(SAM)
-
-
-app = FastAPI(debug=True)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-segmented_mask = []
-interactive_mask = []
-mask_input = None
-
-GLOBAL_IMAGE = None
-GLOBAL_MASK = None
-GLOBAL_ZIPBUFFER = None
 
 sessions = {}
 
+sam_checkpoint = "./models/sam_vit_h_4b8939.pth" # "sam_vit_h_4b8939.pth" or "sam_vit_l_0b3195.pth" or "sam_vit_b_01ec64.pth
+model_type = "vit_h" # "vit_l" or "vit_h"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+device_0 = "cuda:0"
+device_1 = "cuda:1"
+device_2 = "cuda:2"
+device_3 = "cuda:3"
 
-@app.post("/image")
-async def process_images(
-    image: UploadFile = File(...),
-    session_id: str = Form(...),
-):
-    global sessions
-    import os
-    f = "./embedding_cached.pt"
+print("Loading model")
+SAM_0 = sam_model_registry[model_type](checkpoint=sam_checkpoint).to(device_0)
+SAM_1 = sam_model_registry[model_type](checkpoint=sam_checkpoint).to(device_1)
+SAM_2 = sam_model_registry[model_type](checkpoint=sam_checkpoint).to(device_2)
+SAM_3 = sam_model_registry[model_type](checkpoint=sam_checkpoint).to(device_3)
+print("Finishing loading")
 
-    segmented_mask = []
-    interactive_mask = []
+predictor_0 = SamPredictor(SAM_0)
+predictor_1 = SamPredictor(SAM_1)
+predictor_2 = SamPredictor(SAM_2)
+predictor_3 = SamPredictor(SAM_3)
 
-    # Read the image and mask data as bytes
-    image_data = await image.read()
-    image_data = BytesIO(image_data)
-    img = np.array(Image.open(image_data))
-    print("get image", img.shape)
-    GLOBAL_IMAGE = img[:,:,:-1]
+
+if __name__ == "__main__":
     
-    session = sessions.get(session_id)
-    if session is None:
-        session = {
-            "image": GLOBAL_IMAGE,
-            "mask_input": None,
-            "segmented_mask": [],
-            "interactive_mask": [],
-            "zip_buffer": None,
-            "global_mask": None,
-            "features": None
-        }
-        sessions[session_id] = session
-    
-    # produce an image embedding by calling SamPredictor.set_image
-    # if os.path.exists(cache_path):
-    #     predictor.load_embedding(cache_path)
-    #     print(predictor.is_image_set)
-    # else:
-    #     predictor.set_image(GLOBAL_IMAGE)
-    #     predictor.save_embedding(cache_path)
-    #     print("finish setting image")
-    predictor.set_image(sessions[session_id]['image'])
-    sessions[session_id]['features'] = predictor.features
-    
-    # Return a JSON response
-    return JSONResponse(
-        content={
-            "message": "Images received successfully",
-        },
-        status_code=200,
+    app = FastAPI(debug=True)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
 
-@app.post("/undo")
-async def undo_mask(
-    session_id: str = Form(...),
-):
-    global sessions
-    sessions[session_id]["segmented_mask"].pop()
-
-    return JSONResponse(
-        content={
-            "message": "Clear successfully",
-        },
-        status_code=200,
-    )
-
-    
-from fastapi import Request
-@app.post("/click")
-async def click_images(
-    request: Request,
-    session_id: str = Form(...),
-):  
-    global sessions
-
-    session = sessions.get(session_id)
-    
-    form_data = await request.form()
-    type_list = [int(i) for i in form_data.get("type").split(',')]
-    click_list = [int(i) for i in form_data.get("click_list").split(',')]
-
-    point_coords = np.array(click_list, np.float32).reshape(-1, 2)
-    point_labels = np.array(type_list).reshape(-1)
-    
-    # print(point_coords)
-    # print(point_labels)
-    if (len(point_coords) == 1):
-        session['mask_input'] = None
-    predictor.features = session['features']
-    masks_, scores_, logits_ = predictor.predict(
-        point_coords=point_coords,
-        point_labels=point_labels,
-        mask_input=session['mask_input'],
-        multimask_output=True,
-    )
-
-    best_idx = np.argmax(scores_)
-    res = masks_[best_idx]
-    mask_input = logits_[best_idx][None, :, :]
-
-    len_prompt = len(point_coords)
-    len_mask = len(session['interactive_mask'])
-    if len_mask == 0 or len_mask < len_prompt:
-        session["interactive_mask"].append(res)
-    else:
-        session['interactive_mask'][len_prompt-1] = res
-    
-    sessions[session_id] = session
-
-    # Return a JSON response
-    res = Image.fromarray(res)
-    return JSONResponse(
-        content={
-            "masks": pil_image_to_base64(res),
-            "message": "Images processed successfully"
-        },
-        status_code=200,
-    )
-
-@app.post("/finish_click")
-async def finish_interactive_click(
-    mask_idx: int = Form(...),
-    session_id: str = Form(...),
-):
-    global sessions
-
-    session = sessions.get(session_id)
-    
-    session['segmented_mask'].append(session['interactive_mask'][mask_idx])
-    session['interactive_mask'] = list()
-    
-    sessions[session_id] = session
-
-    return JSONResponse(
-        content={
-            "message": "Finish successfully",
-        },
-        status_code=200,
-    )
+    app.include_router(sam_router.router)
 
 
-@app.post("/rect")
-async def rect_images(
-    start_x: int = Form(...), # horizontal
-    start_y: int = Form(...), # vertical
-    end_x: int = Form(...), # horizontal
-    end_y: int = Form(...),  # vertical
-    session_id: str = Form(...)
-):
-    global sessions
-    
-    session = sessions.get(session_id)
+    @app.get("/assets/{path}/{file_name}", response_class=FileResponse)
+    async def read_assets(path, file_name):
+        return f"assets/{path}/{file_name}"
 
-    predictor.features = session['features']
-    masks_, _, _ = predictor.predict(
-        point_coords=None,
-        point_labels=None,
-        box=np.array([[start_x, start_y, end_x, end_y]]),
-        multimask_output=False
-    )
-    
-    res = Image.fromarray(masks_[0])
-    print(masks_[0].shape)
-    # res.save("res.png")
-    # Return a JSON response
-    return JSONResponse(
-        content={
-            "masks": pil_image_to_base64(res),
-            "message": "Images processed successfully"
-        },
-        status_code=200,
-    )
-    
-    
-class PointsGenerator(BaseModel):
-    points_per_side: str
-    points_per_batch: str
-    session_id: str
+    @app.get("/", response_class=HTMLResponse)
+    async def read_index():
+        return read_content('segDrawer.html')
 
-
-@app.post("/everything")
-async def seg_everything(
-    params: PointsGenerator
-):
-    """
-        segmentation : the mask
-        area : the area of the mask in pixels
-        bbox : the boundary box of the mask in XYWH format
-        predicted_iou : the model's own prediction for the quality of the mask
-        point_coords : the sampled input point that generated this mask
-        stability_score : an additional measure of mask quality
-        crop_box : the crop of the image used to generate this mask in XYWH format
-    """    
-    global SAM, sessions
-    # if type(GLOBAL_MASK) != type(None):
-    #     return JSONResponse(
-    #         content={
-    #             "masks": pil_image_to_base64(GLOBAL_MASK),
-    #             "zipfile": b64encode(GLOBAL_ZIPBUFFER.getvalue()).decode("utf-8"),
-    #             "message": "Images processed successfully"
-    #         },
-    #         status_code=200,
-    #     )
-
-    params = dict(params)
-    pps = int(params['points_per_side'])
-    ppb = int(params['points_per_batch'])
-    session_id = params['session_id']
-    
-    session = sessions.get(session_id)
-    
-    mask_generator = SamAutomaticMaskGenerator(model=SAM,
-                                               points_per_side=pps,
-                                               points_per_batch=ppb)
-
-    masks = mask_generator.generate(session['image'])
-    assert len(masks) > 0, "No masks found"
-
-    sorted_anns = sorted(masks, key=(lambda x: x['area']), reverse=True)
-    print(len(sorted_anns))
-
-    # Create a new image with the same size as the original image
-    img = np.zeros((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1]), dtype=np.uint8)
-    for idx, ann in enumerate(sorted_anns, 0):
-        img[ann['segmentation']] = idx % 255 + 1 # color can only be in range [1, 255]
-    
-    res = Image.fromarray(img)
-    session['global_mask'] = res
-
-    # Save the original image, mask, and cropped segments to a zip file in memory
-    zip_buffer = BytesIO()
-    PIL_GLOBAL_IMAGE = Image.fromarray(session['image'])
-    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        # Cut out the segmented regions as smaller squares
-        for idx, ann in enumerate(sorted_anns, 0):
-            left, top, right, bottom = ann["bbox"][0], ann["bbox"][1], ann["bbox"][0] + ann["bbox"][2], ann["bbox"][1] + ann["bbox"][3]
-            cropped = PIL_GLOBAL_IMAGE.crop((left, top, right, bottom))
-
-            # Create a transparent image with the same size as the cropped image
-            transparent = Image.new("RGBA", cropped.size, (0, 0, 0, 0))
-
-            # Create a mask from the segmentation data and crop it
-            mask = Image.fromarray(ann["segmentation"].astype(np.uint8) * 255)
-            mask_cropped = mask.crop((left, top, right, bottom))
-
-            # Combine the cropped image with the transparent image using the mask
-            result = Image.composite(cropped.convert("RGBA"), transparent, mask_cropped)
-
-            # Save the result to the zip file
-            result_bytes = BytesIO()
-            result.save(result_bytes, format="PNG")
-            result_bytes.seek(0)
-            zip_file.writestr(f"seg_{idx}.png", result_bytes.read())
-
-    # move the file pointer to the beginning of the file so we can read whole file
-    zip_buffer.seek(0)
-    session['zip_buffer'] = zip_buffer
-    sessions[session_id] = session
-    
-    # Return a JSON response
-    return JSONResponse(
-        content={
-            "masks": pil_image_to_base64(session['global_mask']),
-            "zipfile": b64encode(session['zip_buffer'].getvalue()).decode("utf-8"),
-            "message": "Images processed successfully"
-        },
-        status_code=200,
-    )
-
-@app.get("/assets/{path}/{file_name}", response_class=FileResponse)
-async def read_assets(path, file_name):
-    return f"assets/{path}/{file_name}"
-
-@app.get("/", response_class=HTMLResponse)
-async def read_index():
-    return read_content('segDrawer.html')
-
-import uvicorn
-uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
